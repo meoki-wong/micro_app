4cc36b465002aa4c6436dad411aed798a72f9958
wangyongfeng <1084415961@qq.com>
2024-04-02 09:50:11 +0800

    feat: 切换自定义webpack

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+//ikun-home/webpack.config.js  
+const HtmlWebpackPlugin = require("html-webpack-plugin");  
+  
+module.exports = {  
+    entry: "./src/index",  
+    mode: "development",  
+    devServer: {  
+        port: 3000, 
+    },  
+    module: {  
+        rules: [  
+            {  
+                test: /\.(js|jsx)?$/,  
+                exclude: /node_modules/,  
+                use: [  
+                    {  
+                        loader: "babel-loader",  
+                        options: {  
+                            presets: ["@babel/preset-env", "@babel/preset-react"],  
+                        },  
+                    },  
+                ],  
+            },  
+            {  
+                test: /\.css$/i,  
+                use: ["style-loader", "css-loader"],  
+            },  
+        ],  
+    },  
+    plugins: [  
+        new HtmlWebpackPlugin({  
+            template: "./public/index.html",  
+        }),  
+    ],  
+    resolve: {  
+        extensions: [".js", ".jsx"],  
+    },  
+    target: "web",  
+};
